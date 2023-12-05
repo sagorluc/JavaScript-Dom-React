9ac160cbf242919661470899eec2e5c6e378f31d
sagorluc <mdsagorluc@gmail.com>
2023-12-05 19:11:25 +0600

    connect tailwind css with reactJS

---
+++ 06GeedyShowwsReact/02ConnectTailwindcssWithReact/src/App.jsx
@@ -0,0 +1,15 @@
+
+
+
+function App() {
+
+  return (
+    // Blank Fragment
+    <>
+      <h1 className="ext-3xl font-bold underline">Connect Tailwind css with React</h1>
+
+    </>
+  )
+}
+
+export default App
